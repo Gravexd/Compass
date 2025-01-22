17fb5cec1b6a42469cde5a67740ff9dfbf199d85
Gravxd <mikey643214@gmail.com>
2025-01-21 23:55:43 -0500

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'cerulean'
+game 'gta5'
+
+version '2.0.0'
+description 'https://github.com/thelindat/compass'
+
+
+client_scripts {
+	"config.lua",
+	"essentials.lua",
+	"client/compass_cl.lua",
+}
+
+ui_page('html/index.html')
+
+files({
+    'html/index.html',
+    'html/app.js',
+    'html/style.css',
+})
+
+shared_script '@ox_lib/init.lua'
+
+lua54 "yes"
